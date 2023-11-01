ab7b211e5e1a76b634346546e1d065259240cd3e
sz <698307+xparq@users.noreply.github.com>
2023-11-01 02:53:30 +0100

    Adding test for op("empty") with --empty=

---
+++ test/empty-arg query.case/empty-arg query.cpp
@@ -0,0 +1,9 @@
+﻿#include "Args.hpp"
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv)
+{
+	Args args(argc, argv);
+	if (args("arg") == "") cout << "arg is empty";
+	else cout << "arg is: " << args("arg") << "\n";
+}
